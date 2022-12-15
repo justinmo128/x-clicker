c3d4861632494c7f44ec4c9f7c8ff94366899b16
justinmo128 <j.mo1@share.epsb.ca>
2022-12-15 11:01:54 -0700

    cookie basics

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-document.getElementById("xbutton").addEventListener("click", click)
-let x = 0;
-let output = document.getElementById("xcount")
+let x = getCookie("x");
+let cursors = getCookie("cursors");
+let cursorOut = document.getElementById("cursor-amt");
+let output = document.getElementById("xcount");
+output.innerHTML = x;
+cursorOut.innerHTML = `${cursors} (${(cursors / 5).toFixed(1)}X/second)`;
 
+document.addEventListener("click", updateCookies);
+function updateCookies() {
+    document.cookie = `x=${x}`;
+    document.cookie = `cursors=${cursors}`;
+}
+
+document.getElementById("xbutton").addEventListener("click", click);
 function click() {
     x++;
     output.innerHTML = x;
 }
 
-let cursors = 0;
-let cursorOut = document.getElementById("cursor-amt")
 document.getElementById("buy-cursor").addEventListener("click", buyCursor)
 function buyCursor() {
     if (x >= 15) {
@@ -18,4 +26,20 @@ function buyCursor() {
         setInterval(() => {x++; output.innerHTML = x;}, 5000)
         output.innerHTML = x;
     }
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
 }
